3a1598f6c71082ec15a876b9a4c6bf6dd95969ec
Safin Singh <safin.singh@gmail.com>
2020-08-30 11:27:02 -0700

    command exit code

--- src/checks.rs
+++ src/checks.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::fs;
+use std::process::Command;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct FileContains {
@@ -19,5 +20,43 @@ impl FileContains {
             return true;
         }
         false
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CommandExitCode {
+    command: String,
+    code: Option<i32>,
+}
+
+impl CommandExitCode {
+    pub fn query(&self) -> bool {
+        let mut args = self.command.split(' ');
+        let cmd = args.next().unwrap();
+
+        let code = Command::new(cmd).args(args).status();
+        let err = match code {
+            Ok(c) => c,
+            Err(_) => return false,
+        };
+
+        match err.code() {
+            Some(e) => {
+                if let Some(c) = self.code {
+                    if e == c {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if e == 0 {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            None => return false,
+        }
     }
 }
